fc306acd095a6ecc3e4c6543d631731c170882da
phelisa2020 <phelisagqwetha@gmail.com>
2020-08-26 16:20:36 +0200

    first commits

---
+++ settings-bill.js
@@ -0,0 +1,93 @@
+module.exports = function settingsBill(){
+	var callCost = 0
+	var smsCost = 0
+	var warningLevel = 0
+	var criticalLevel = 0
+	
+	let actionList = []
+
+	function setSettings(settings){
+    callCost = Number(settings.callCost); 
+    smsCost = Number(settings.smsCost);
+    warningLevel = settings.warningLevel; 
+    criticalLevel = settings.criticalLevel;
+      
+}
+function getSettings(){
+	return {
+		callCost,
+		smsCost,
+		warningLevel,
+		criticalLevel
+	}
+}
+
+function recordAction(action){
+
+	let cost = 0
+	if(action === 'sms'){
+		cost = smsCost
+	}
+	else if(action === 'call'){
+		cost = callCost
+	}
+	actionList.push({
+		type: action,
+		cost,
+		timestamp: new Date()
+	});
+}
+	function actions(){
+		return actionList
+	}
+	
+	function actionsfor(type){
+		return actionList.filter((action) => action.type === type)
+
+	}
+	function getTotal(type){
+		return actionList.reduce((total, action) => {
+			let val = action.type === type ? action.cost :0
+			return total + val
+		}, 0);
+
+	}
+	function grandTotal(){
+		return getTotal('sms') + getTotal('call');
+	}
+	function totals(){
+		let smsTotal = getTotal('sms')
+		let callTotal = getTotal('call')
+		return{
+			smsTotal,
+			callTotal,
+			grandTotal : grandTotal()
+		}
+
+	}
+
+function hasReachedWarningLevel(){
+	const total = grandTotal()
+	const reachedWarningLevel = total >= warningLevel 
+	&& total < criticalLevel;
+	return reachedWarningLevel;
+}
+
+function hasReachedCriticalLevel(){
+	const total = grandTotal()
+	
+	return total >= criticalLevel
+}
+ 	
+	return {
+		setSettings,
+		getSettings,
+		recordAction,
+		actions,
+		actionsfor,
+		totals,
+		hasReachedCriticalLevel,
+		hasReachedWarningLevel
+
+	}
+}
